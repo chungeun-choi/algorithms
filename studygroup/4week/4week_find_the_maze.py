c479b6ec38c07a538182d85fc4d82fd9e78da4ab
cucuridas@gamil.com <3310223@naver.com>
2023-08-17 15:26:14 +0900

    solved task - programmers_find the maze

--- studygroup/4week/4week_find_the_maze.py
+++ studygroup/4week/4week_find_the_maze.py
@@ -1,40 +1,65 @@
+def dfs(start,end,maps):
+    width = len(maps[0])
+    hight = len(maps)
+    visited = [[0]*width for i in range(hight)]
+    moving = [(-1,0),(1,0),(0,-1),(0,1)]
+    queue = [start]
+    visited[start[0]][start[1]] = 1
+
+    while queue:
+        x,y = queue.pop()
+        
+        for dx,dy in moving:
+            nx, ny = x+dx,y+dy
+
+            if 0 <= nx < hight and 0 <= ny < width:
+                if maps[nx][ny] != "X" and visited[nx][ny] ==0:
+                    if (nx,ny) == end:
+                        return visited[x][y]
+                    visited[nx][ny] = visited[x][y] +1
+                    queue.append((nx,ny))
+                
+    return -1
+
+
 from collections import deque
+
+def bfs(start,end,maps):
+    width = len(maps[0])
+    hight = len(maps)
+    visited = [[0]*width for i in range(hight)]
+    moving = [(-1,0),(1,0),(0,-1),(0,1)]
+    queue = deque([start])
+    visited[start[0]][start[1]] = 1
+
+    while queue:
+        x,y = queue.popleft()
+        
+        for dx,dy in moving:
+            nx, ny = x+dx,y+dy
+
+            if 0 <= nx < hight and 0 <= ny < width:
+                if maps[nx][ny] != "X" and visited[nx][ny] ==0:
+                    if (nx,ny) == end:
+                        return visited[x][y]
+                    visited[nx][ny] = visited[x][y] +1
+                    queue.append((nx,ny))
+                
+    return -1
+
 # 직사각형인데 문제 똑바로 안 읽고 정사각형인 줄 알고 풀었다가 틀림....
 def solution(maps):
-    def bfs(start,end):
-        visited = [[0]*wid for i in range(hei)]
-        queue = deque([start])
-        dx = [0, 0, -1, 1]
-        dy = [-1, 1, 0, 0]
-        visited[start[0]][start[1]] = 1
 
-        while queue:
-            x,y = queue.popleft()
-            for cnt in range(0,4):
-                nx = x + dx[cnt]
-                ny = y + dy[cnt]
-                
-                if nx < 0 or ny < 0 or nx >= hei or ny >= wid:
-                    continue
-                if maps[nx][ny] == "X" or visited[nx][ny] != 0:
-                    continue
-                visited[nx][ny] = visited[x][y] +1
-                queue.append((nx,ny))
-        return  visited[end[0]][end[1]] -1
-    
-    hei = len(maps)
-    wid = len(maps[0])
-
-    for i in range(hei):
-        for j in range(wid):
+    for i in range(len(maps)):
+        for j in range(len(maps[0])):
             if maps[i][j] == "S":s_pos = (i,j)
             elif maps[i][j] == "L" : l_pos = (i,j)
             elif maps[i][j] == "E" : e_pos = (i,j)
     
-    l_distacne = bfs(s_pos,l_pos)
+    l_distacne = dfs(s_pos,l_pos,maps)
     if l_distacne == -1:
         return -1
-    e_distance = bfs(l_pos,e_pos)
+    e_distance = dfs(l_pos,e_pos,maps)
     if e_distance == -1:
         return -1
 
