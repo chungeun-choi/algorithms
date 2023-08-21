5d8c3b00d0e11b76a381d20d8a64b0754a27d217
cucuridas@gamil.com <3310223@naver.com>
2023-08-21 17:35:11 +0900

    solved task by using go lang - programmers_maze

--- studygroup/4week/go_find_maze/main.go
+++ studygroup/4week/go_find_maze/main.go
@@ -1,26 +1,94 @@
-package gofindmaze
+package main
 
 import (
 	"fmt"
+	"unicode/utf8"
 )
 
-type Position struct {
+type Pos struct {
 	x int
 	y int
-
 }
 
-func main() {
-	queue := make(chan Position, 10000)
-	value := &Position{
-		x: 123,
-		y: 22,
+func bfs(start *Pos, end *Pos,maps []string) int {
+	queue := make(chan *Pos,10000)
+	var moving = [4]Pos{
+		{x: -1, y:0},
+		{x: 1 , y:0},
+		{x: 0, y:-1},
+		{x:0, y:1},
 	}
-	queue <- *value
+	width := utf8.RuneCountInString(maps[0])
+	hight := len(maps)
+	
+	visited := make([][]int, hight)
+	for i := range visited {
+		visited[i] = make([]int, width)
+	}
 
-	//var result Position
+	queue <- start
+	visited[start.x][start.y] = 1
+
+	for position := range queue{
+		x := position.x
+		y := position.y
+		
+		for _,value := range moving{
+			nx := x + value.x
+			ny := y + value.y
+
+			if 0 <= nx && nx < hight && 0 <= ny && ny < width {
+				if visited[nx][ny] == 0{
+					if x_value := maps[nx]; x_value[ny] != 'X'{
+						positions := &Pos{
+							x: nx,
+							y:ny,
+						}
+						if positions.x == end.x && positions.y == end.y {
+							return visited[x][y]
+						}else{
+							visited[nx][ny] = visited[x][y] +1
+							queue <- positions
+						}
+					}
+
+				}
+			}
+		}
+
+	}
+	return -1
+}
+
+
+func solution(maps []string) int {
+	// 시작 위치, 레버 위치, 탈출 위치 찾기
+
+	// bfs를 통해 시작위치에서 레버위치까지 거리
+	// 레버위치에서 탈출 위치까지의 거리 찾기
+	// 거리에서 return 하는 값이 없을 경우 탈출 경로가 없다 판단하여 return -1
+	test_start := &Pos{
+		x: 0,
+		y: 0,
+	}
+	test_lever := &Pos{
+		x: 0,
+		y: 4,
+	}
+	result := bfs(test_start,test_lever,maps)
+	fmt.Println(result)
+    return 0
+}
+
+
+
+
+func main() {
+	testInput := [5]string{"SOOOL","XXXXO","OOOOO","OXXXX","OOOOE"}
+
+	solution(testInput[:])
+
 
 	
-	fmt.Println(<- queue)
-	
+
 }
