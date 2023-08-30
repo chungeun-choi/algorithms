fa9518d91a08c8a8fe5f53bc55ea803a418405c2
cucuridas@gamil.com <3310223@naver.com>
2023-08-30 13:52:40 +0900

    solving task - programmers_number_sub_sequence

---
+++ studygroup/5week/Number_Sub_Sequence.py
@@ -0,0 +1,32 @@
+from itertools import permutations
+
+def solution(elements):
+    answer = 0
+    leng_elements = len(elements)
+
+    for count in range(1, leng_elements+1):
+        if count == 1:
+            answer += len(set(elements))
+            continue
+        if count == leng_elements:
+            answer += leng_elements
+            continue
+        else:
+            check_list = set()
+            for count2 in range(0,leng_elements):
+                range_value = count+count2
+                if range_value > leng_elements:
+                    value = sum(elements[count2:leng_elements]) + sum(elements[:abs(leng_elements -(range_value))])
+                    check_list.add(value)
+                check_list.add(sum(elements[count2:range_value]))
+            answer += len(check_list)
+            
+        
+        
+    return answer
+
+
+if __name__ == "__main__":
+    elements = [7,9,1,1,4]
+
+    assert(solution(elements=elements)==18)
