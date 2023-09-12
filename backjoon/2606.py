2ca5ee00c2ffcba7bb13290c1e96bac0c292a7dc
cucuridas@gamil.com <3310223@naver.com>
2023-09-12 14:18:52 +0900

    solved task - backjoon 2606

---
+++ backjoon/2606.py
@@ -0,0 +1,35 @@
+
+if __name__ == "__main__":
+    node = int(input())
+    degree = int(input())
+    graph = dict(zip([i for i in range(1,node+1)],[[] for i in range(node)]))
+
+    for i in range(degree):
+        s_node, d_node = map(int,input().split())
+        graph[s_node].append(d_node)
+        graph[d_node].append(s_node)
+
+
+    for _,value in graph.items():
+        value.sort()
+
+    def dfs(graph,start):
+        keys = graph.keys()
+        visited = dict(zip(keys,[0 for i in range(len(keys))]))
+        stack = [start]
+        visited[start] = 1
+        count = 0
+
+        while stack:
+            node = stack.pop()
+
+            for i in graph[node]:
+                if visited[i] == 0:
+                    visited[i] =1
+                    count +=1
+                    stack.append(i)
+            
+        return count
+
+    
+    print(dfs(graph,1))
