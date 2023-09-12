cb47ae0c9d9fc0f3921bfb73a55df012d0d53d69
cucuridas@gamil.com <3310223@naver.com>
2023-09-12 13:12:42 +0900

    sovled task - backjoon 1260

---
+++ backjoon/1260.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+def bfs(graph,start,visited):
+    queue = deque([(start)])
+
+    while queue:
+        node = queue.popleft()
+        if visited[node] == 0:
+            queue.extend(graph[node])
+            visited[node] = 1
+            print(node,end= " ")
+        else:
+            continue
+
+def dfs(graph,start,visited):
+
+    print(start,end=" ")
+
+    for i in graph[start]:
+        if visited[i] == 0:
+            visited[i] =1
+            dfs(graph,i,visited)
+
+
+if __name__ == "__main__":
+    len_node, degree, start = map(int,input().split())
+
+    graph = dict(zip([i for i in range(1,len_node+1)],[[] for i in range(len_node)]))
+
+    for i in range(degree):
+        node_s,node_d = map(int,input().split())
+        graph[node_s].append(node_d)
+        graph[node_d].append(node_s)
+    
+    for key,value in graph.items():
+        value.sort()
+
+    visited =dict(zip([i for i in range(1,len_node+1)],[0 for i in range(len_node)]))
+    bfs(graph,start,visited) 
+    print("")
+    visited =dict(zip([i for i in range(1,len_node+1)],[0 for i in range(len_node)]))
+    visited[start] = 1
+    dfs(graph,start,visited)
+    
