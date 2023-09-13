c2c5cbdbb0c1b72495e7871ddb2594086692f96b
cucuridas@gamil.com <3310223@naver.com>
2023-09-13 15:02:33 +0900

    solved task - baekjoon 11399

---
+++ backjoon/11399.py
@@ -0,0 +1,16 @@
+if __name__ == "__main__":
+    N = int(input())
+    LIST = list(map(int,input().split()))
+    answer = 0
+    LIST.sort()
+
+    for index in range(N):
+        if index == 0:
+            answer += LIST[index]
+        else:
+            LIST[index] = LIST[index] + LIST[index-1]
+            answer += LIST[index]
+
+    print(answer)
+        
+    
